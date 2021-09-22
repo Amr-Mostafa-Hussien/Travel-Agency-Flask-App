a2c80ec71bec404f100373623edace391fa2b5c5
Amr-Mostafa-Hussien <70381944+Amr-Mostafa-Hussien@users.noreply.github.com>
2021-09-22 23:25:32 +0200

    Initial Commit

---
+++ tv/default.py
@@ -0,0 +1,38 @@
+
+DEFAULT_SOURCE = """
++ hello
+- hello
++ my name is *
+- <set name=<formal>>Nice to meet you, <get name>.
+
++ do you have any discounts right now
+- sure
+
++ can you tell me the bigger one
+- we have a 20 % discount for a trip in europe
+
++ great thank you
+- can i help you with something else
+
++ *
+- i don't have a reply for that.
+- try asking that a different way.
+
++ i want to take a vacation
+- to where
+
++ i want to book a vacation to europe
+- there is a trip available to italy and spain
+
++ i choose italy
+- what is the date you want to take off
+
++ after 1 week
+- how many persons
+
++ five
+- can you please submit your name and your phone number
+
++ my phone is *
+- flight tickets reserved.
+"""
